44c3c48ac542034b39801a2c62bb38a15a9ae513
Gram <master_fess@mail.ru>
2017-11-13 16:11:47 +0500

    +example skeleton

---
+++ example/users/views.py
@@ -0,0 +1,17 @@
+import djburger
+
+
+class IndexView(djburger.ViewBase):
+    pass
+
+
+class UserCommonAPIView(djburger.ViewBase):
+    pass
+
+
+class UserActionsAPIView(djburger.ViewBase):
+    pass
+
+
+class UserManagementAPIView(djburger.ViewBase):
+    pass
