d2c276aabbca136effb2469b047efbd93a6e233d
orsinium <master_fess@mail.ru>
2017-11-19 12:20:17 +0500

    +docstrings 4 controllers

--- djburger/controllers.py
+++ djburger/controllers.py
@@ -12,8 +12,22 @@ __all__ = [
 
 
 class ListController(ListView):
+    """Controller based on Django ListView
+
+    1. Get list of objects
+    2. Filter list by validated data from user
+    3. Optional pagination
+    """
 
     def __init__(self, only_data=True, **kwargs):
+        """Initialize controller in rule.
+
+        Args:
+            only_data (bool): return only filtered queryset if True,
+                all context data otherwise. Use `only_data=False` with
+                TemplateSerializerFactory.
+            **kwargs: all arguments of ListView.
+        """
         self.only_data = only_data
         super(ListController, self).__init__(**kwargs)
 
@@ -27,7 +41,7 @@ class ListController(ListView):
 
     def get_context_data(self, **kwargs):
         context = super(ListController, self).get_context_data(**kwargs)
-        # возвращаем только список объектов
+        # return only filtered queryset
         if self.only_data:
             return context['object_list']
         return context
@@ -37,17 +51,36 @@ class ListController(ListView):
 
 
 class InfoController(object):
+    """Return one object from queryset
+
+    1. Return object filtered by params from URL kwargs  (like `pk` or `slug`)
+        if url-pattern have kwargs.
+    2. Return object from validated data if data have key `object` 
+        or data have only one key.
+    3. Raise exception otherwise.
+    """
 
     def __init__(self, queryset):
         self.q = queryset
 
     def __call__(self, request, data, **kwargs):
-        return get_object_or_404(self.q, **kwargs)
+        if kwargs:
+            return get_object_or_404(self.q, **kwargs)
+        elif 'object' in data:
+            return data['object']
+        elif len(data) == 1:
+            return data.pop()
+        else:
+            raise ValueError("Can't select object!")
 
 
 class AddController(object):
+    """Controller for adding object with validated data.
+    """
 
     def __init__(self, model):
+        """Initialize controller with passed model.
+        """
         self.model = model
 
     def __call__(self, request, data, **kwargs):
@@ -55,6 +88,12 @@ class AddController(object):
 
 
 class EditController(object):
+    """Controller for editing objects.
+
+    1. Get object of initialized model by URL's kwargs.
+    2. Set params from validated data.
+    3. Update tuple into database.
+    """
 
     def __init__(self, model, filters=None):
         self.model = model
@@ -66,6 +105,10 @@ class EditController(object):
 
 
 class DeleteController(object):
+    """Controller for deleting objects.
+
+    Delete object filtered by URL's kwargs.
+    """
 
     def __init__(self, model, filters=None):
         self.model = model
@@ -76,15 +119,35 @@ class DeleteController(object):
 
 
 class ViewAsController(object):
+    """Allow use any django view as controller.
+    
+    1. For CBV with render_to_response method return context.
+    2. Return rendered response otherwise.
+    """
 
     def __init__(self, view, method=None):
-        self.view = view
+        self.view = self.patch_view(view)
         self.method = method
 
+    @staticmethod
+    def patch_view(view):
+        """Patch view for getting context instead of rendered response.
+        """
+        if hasattr(view, 'render_to_response'):
+            view.render_to_response = lambda context, **kwargs: context
+        return view
+
     def __call__(self, request, data, **kwargs):
+        # set data to request
         method = self.method or request.method
         if method.lower() == 'get':
             request.GET = data
         else:
             request.POST = data
-        return self.view(request=request, **kwargs)
+
+        # get response
+        response = self.view(request=request, **kwargs)
+        if hasattr(response, 'content'):
+            return response.content
+        else:
+            return response
